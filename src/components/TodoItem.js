6f1a6edf2a7e073df6aecb65ce520bd5c87a27d7
David-Kasilia <davidkasilia001@gmail.com>
2022-09-12 22:11:44 +0300

    Part 2 tutorial complete

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,7 +1,17 @@
-import React from "react"
+import React from "react";
 
 function TodoItem(props) {
-  return <li>{props.todo.title}</li>
+  return (
+    <li>
+      <input
+        type="checkbox"
+        checked={props.todo.completed}
+        onChange={() => props.handleChangeProps(props.todo.id)}
+      />
+      <button onClick={() => props.deleteTodoProps(props.todo.id)}>Delete</button>
+      {props.todo.title}
+    </li>
+  );
 }
 
-export default TodoItem
+export default TodoItem;
