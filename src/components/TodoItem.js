0ef29dcba2017a3c5d23ab446e6b1c4dd8ffcc46
David-Kasilia <davidkasilia001@gmail.com>
2022-09-12 22:19:41 +0300

    Part 3 tutorial complete

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,15 +1,30 @@
 import React from "react";
+import styles from "./TodoItem.module.css";
 
 function TodoItem(props) {
+  const completedStyle = {
+    fontStyle: "italic",
+    color: "#595959",
+    opacity: 0.4,
+    textDecoration: "line-through",
+  };
+
+  const { completed, id, title } = props.todo
+
   return (
-    <li>
+    <li className={styles.item}>
       <input
         type="checkbox"
-        checked={props.todo.completed}
-        onChange={() => props.handleChangeProps(props.todo.id)}
+        className={styles.checkbox}
+        checked={completed}
+        onChange={() => props.handleChangeProps(id)}
       />
-      <button onClick={() => props.deleteTodoProps(props.todo.id)}>Delete</button>
-      {props.todo.title}
+      <button onClick={() => props.deleteTodoProps(id)}>
+        Delete
+      </button>
+      <span style={props.todo.completed ? completedStyle : null}>
+        {title}
+      </span>
     </li>
   );
 }
