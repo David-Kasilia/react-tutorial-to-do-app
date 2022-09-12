034f91c824dbfa616509105e66146cef6ea193c0
David-Kasilia <davidkasilia001@gmail.com>
2022-09-12 22:52:17 +0300

    Part 5 and 6 tutorial complete

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -7,23 +7,25 @@ import { v4 as uuidv4 } from "uuid";
 class TodoContainer extends React.Component {
   state = {
     todos: [
-      {
-        id: uuidv4(),
-        title: "Setup development environment",
-        completed: true,
-      },
-      {
-        id: uuidv4(),
-        title: "Develop website and add content",
-        completed: false,
-      },
-      {
-        id: uuidv4(),
-        title: "Deploy to live server",
-        completed: false,
-      },
+
     ],
   };
+  componentDidMount() {
+    const temp = localStorage.getItem("todos")
+    const loadedTodos = JSON.parse(temp)
+    if (loadedTodos) {
+      this.setState({
+        todos: loadedTodos
+      })
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if(prevState.todos !== this.state.todos) {
+      const temp = JSON.stringify(this.state.todos)
+      localStorage.setItem("todos", temp)
+    }
+  }
 
   setUpdate = (updatedTitle, id) => {
     this.setState({
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -16,6 +16,10 @@ class TodoItem extends React.Component {
       }
   }
 
+  componentWillUnmount() {
+    console.log("Cleaning up...")
+  }
+
   render() {
     const completedStyle = {
       fontStyle: "italic",
